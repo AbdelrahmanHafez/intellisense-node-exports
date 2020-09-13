73428c644a24ee83f7da749cc10e233f7f277811
Hafez <a.hafez852@gmail.com>
2020-09-14 01:26:32 +0200

    Initial commit

---
+++ index.js.js
@@ -0,0 +1,5 @@
+const add = require('./subpath/add.js');
+
+const sum = add(1, 2);
+
+console.log(`Sum is: ${sum}.`);
---
+++ subpath/add.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ * @param {Number} x 
+ * @param {Number} y 
+ * @returns {Number} the sum of two added numbers
+ */
+function add(x, y) {
+  return x + y
+}
+
+module.exports = add;
